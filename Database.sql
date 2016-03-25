56cc66c09d7ca550ddb30091b3c8a744211767a5
mstoldt <mstoldt@gmail.com>
2016-03-26 01:53:07 +0200

    Save the Database

--- Database.sql
+++ Database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 25. Mrz 2016 um 13:56
+-- Erstellungszeit: 26. Mrz 2016 um 00:52
 -- Server-Version: 10.1.10-MariaDB
 -- PHP-Version: 5.6.19
 
@@ -17,118 +17,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `Events`
---
-
-CREATE TABLE `Events` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `name` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `lc` int(11) NOT NULL,
-  `start_date` date NOT NULL,
-  `end_date` date NOT NULL,
-  `description` text NOT NULL,
-  `announce_date` date NOT NULL,
-  `deadline_date` date NOT NULL,
-  `participants_announce_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Daten für Tabelle `Events`
---
-
-INSERT INTO `Events` (`id`, `name`, `lc`, `start_date`, `end_date`, `description`, `announce_date`, `deadline_date`, `participants_announce_date`) VALUES
-(1, 'IT Sprint II', 1, '2016-03-20', '2016-03-27', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. \r\n\r\nDuis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. \r\n\r\nUt wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. ', '2016-01-19', '2016-02-16', '2016-03-07');
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `event_participants`
---
-
-CREATE TABLE `event_participants` (
-  `event_id` int(11) NOT NULL,
-  `person_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Daten für Tabelle `event_participants`
---
-
-INSERT INTO `event_participants` (`event_id`, `person_id`) VALUES
-(1, 1),
-(1, 1),
-(1, 2);
-
--- --------------------------------------------------------
-
---
 -- Tabellenstruktur für Tabelle `LCs`
 --
 
 CREATE TABLE `LCs` (
   `id` int(10) UNSIGNED NOT NULL,
   `city` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `password` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+  `password` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `email` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Daten für Tabelle `LCs`
 --
 
-INSERT INTO `LCs` (`id`, `city`, `password`) VALUES
-(1, 'Patras', ''),
-(2, 'Hamburg', '');
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `login`
---
-
-CREATE TABLE `login` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `mail` text NOT NULL,
-  `password` text NOT NULL,
-  `role` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Daten für Tabelle `login`
---
-
-INSERT INTO `login` (`id`, `mail`, `password`, `role`) VALUES
-(1, 'violeta', '', 'admin');
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `Persons`
---
-
-CREATE TABLE `Persons` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `name` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `eestec_profile_link` text NOT NULL,
-  `lc` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Daten für Tabelle `Persons`
---
-
-INSERT INTO `Persons` (`id`, `name`, `eestec_profile_link`, `lc`) VALUES
-(1, 'Sofia xx', 'eestec.net/xx', 1),
-(2, 'Moritz', 'eestec.net/moritz', 2);
+INSERT INTO `LCs` (`id`, `city`, `password`, `email`) VALUES
+(1, 'Patras', '1234', 'patras@eestec.net'),
+(2, 'Hamburg', '1234', 'hamburg@eestec.net'),
+(3, 'Athens', '', ''),
+(4, 'Krakow', '', '');
 
 --
 -- Indizes der exportierten Tabellen
 --
-
---
--- Indizes für die Tabelle `Events`
---
-ALTER TABLE `Events`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indizes für die Tabelle `LCs`
@@ -137,38 +48,11 @@ ALTER TABLE `LCs`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indizes für die Tabelle `login`
---
-ALTER TABLE `login`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indizes für die Tabelle `Persons`
---
-ALTER TABLE `Persons`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT für exportierte Tabellen
 --
 
 --
--- AUTO_INCREMENT für Tabelle `Events`
---
-ALTER TABLE `Events`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
 -- AUTO_INCREMENT für Tabelle `LCs`
 --
 ALTER TABLE `LCs`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT für Tabelle `login`
---
-ALTER TABLE `login`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT für Tabelle `Persons`
---
-ALTER TABLE `Persons`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
